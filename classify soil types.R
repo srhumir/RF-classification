51a1191f279ff77834486f09eb1c877c6f58e8ba
Reza <srh.umir@gmail.com>
2016-05-16 22:09:21 +0200

    traiing clustered with hclust to 9 classes and rf model trained

--- classify soil types.R
+++ classify soil types.R
@@ -1,10 +1,3 @@
-r <- matrix(1:7, 10,10)
-r <- raster(r)
-r
-par(mfrow = c(1, 2))
-plot(r)
-r2 <- r * (r > 5)
-r3 <- r %/% 6
 #get classifcation rasters of soil vs urban and soil vs roof. 
 #gather soils of both
 #classify soil types
@@ -35,9 +28,87 @@ sumNA <- function(a){
 }
 #use function above to produce soil raster (i.e 1 at soil pixels and NA elswhere)
 soil <- calc(stack(soilurban, soilroof), sumNA)
-#convert to ploygon
-shape <- rasterToPolygons(soil)
-#extract 
-values <- extract(MNF, shape, df = TRUE)
+#load MNF
+MNF <- stack(file.choose())
+MNF <- dropLayer(MNF, c(5,6))
+names(MNF) <- sapply(1:dim(MNF)[3], function(i) paste("MNF", i, sep="."))
+rasterlist <- list(raster(MNF, layer = 1), raster(MNF, layer = 2), 
+                   raster(MNF, layer = 3), raster(MNF, layer = 4))
+#load Reflectance
+Reflectance <- stack(file.choose())
+#load thermal bands
+thermal <- stack(file.choose())
+#compute related ratios
+ironoxid <- raster(Reflectance, layer=4)/raster(Reflectance, layer=2)
+clay  <- raster(Reflectance, layer=6)/raster(Reflectance, layer=7)
+ratio3 <- raster(Reflectance, layer=7)/raster(Reflectance, layer=3)
+ratio4 <- raster(Reflectance, layer=2)/raster(Reflectance, layer=3)
+quartz <- raster(thermal, layer=1) #first thermal band (band 10)
+#last ratio have different extent
+#crop
+quartz <- crop(quartz, ratio4)
 
-unique(getValues(soil))
+
+  
+ratioList <- list(ironoxid, clay, ratio3, ratio4, quartz)
+#stack first four MNF layers and ratios
+toclass <- stack(c(rasterlist, ratioList))
+#mask by soil
+toclass2 <- toclass * soil
+###convert to ploygon
+####shape <- rasterToPolygons(soil)
+
+#trainigs data 
+xx <- readShapePoly(file.choose())
+projection(xx) <- projection(MNF)
+#extract 
+values <- extract(toclass, xx, df = TRUE)
+#take out the attributr table of the trainig and assigne ID to polygons
+classes <- data.frame(ID = 1:length(xx@data$CLASS_NAME), xx@data)
+#Assign class to extracted values
+values <- data.frame(Class = classes$CLASS_NAME[values$ID], values)
+##when load from disk
+#values <- read.csv(file.choose())
+# values <- values[,-1]
+#drop rows with all zero MNF
+values <- values[values[names(values)[3]] > 0 & !is.na(values[names(values)[3]]),]
+##a backup
+valuesBackup <- values
+##No need for ID
+values <- values[,-2]
+## keep class seperate for speeding up the training and ...
+Class <- values$Class
+#check fot too many NA
+NAs <- sapply(1:dim(toclass2)[3],function(i) sum(is.na(getValues(subset(toclass2,i)))))
+#convert inf to NA for the model to work
+for (i in 1:dim(values)[1]){
+  for (j in 1:dim(values)[2]){
+    if (is.infinite(values[i,j])){values[i,j] <- NA}
+  }
+}
+#fill NAs
+##model
+Nafill <- preProcess(values, method = "bagImpute")
+##fill
+valuesNAfilled <- predict(Nafill, values)
+##check
+sum(is.na(valuesNAfilled))
+
+#normalize the data frame to have comparable data
+normal <- preProcess(valuesNAfilled[,-1])
+valuesNormal <- predict(normal, valuesNAfilled[,-1])
+distances <- dist(valuesNormal)
+clus <- hclust(distances)
+plot(clus)
+#
+cut <- cutree(clus, h=6)
+table(Class, cut)
+
+
+#train RF model
+train_control <- trainControl(method="cv", number=10)
+
+system.time(
+modelrf <- train(valuesNAfilled[,-1], factor(cut), trControl=train_control, method = "rf")
+)
+
